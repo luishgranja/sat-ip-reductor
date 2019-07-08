07536c89cc1b2903a484e8ea39060b50af09a3c7
Luis Granja <luis.granja@correounivalle.edu.co>
2019-07-08 00:20:51 -0500

    refactor: using string formatting string formatting to make the code more readable

--- SAT-IP/Reductor/main.py
+++ SAT-IP/Reductor/main.py
@@ -9,7 +9,8 @@ def header_reader(line):
     aux = line[0]
     # Se verifica si la linea es un comentario
     if(aux == 'c'):
-        comments += '% ' + line.split('c')[1] + '\n'
+        aux_comment = line.split('c')[1]
+        comments += f'% {aux_comment}\n'
         s.write(comments)
     # Se verifica si la linea es 'p'
     elif(aux == 'p'):
@@ -26,16 +27,17 @@ def clausoles(line):
     line_aux = line.split()
     for x in range(0, len(line_aux)-1):
         if int(line_aux[x]) > 0: # Verificar si el numero es positivo para ver que nombre de variable se le da
+            var = str(line_aux[x])
             if x == len(line_aux)-2: # Verificar si es la ultima clausula para poner el ; y >=1
-                c += 'v' + str(line_aux[x]) + ' >= 1;\n'
+                c += f'v{var} >= 1;\n'
             else:
-                c += 'v' + str(line_aux[x]) + ' + '
+                c += f'v{var} + '
         else:
             var = int(line_aux[x]) * -1
             if x == len(line_aux)-2:
-                c += 'n_v' + str(var) + ' >= 1;\n'
+                c += f'n_v{var} >= 1;\n'
             else:
-                c += 'n_v' + str(var) + ' + '
+                c += f'n_v{var} + '
     return c
 
 # Construir literales y constraints
@@ -45,11 +47,12 @@ def literals_and_constraints(n_literals):
     global output
     output = 'output [' # Variable para guardar el output del resultado en la conversion de minizinc
     for x in range(1, n_literals+1): # Ciclo para construir los literales, sus negaciones y los constraints de cada literal
-        literals += 'var 0..1: v' + str(x) + '; var 0..1: n_v'+ str(x) +';\n'
-        constraints += 'constraint v'+ str(x) + ' + n_v' + str(x) + ' = 1;\n'
-        output += '"v' + str(x) + '=" ,show(v' + str(x) + '), "\\t-v' + str(x) + '=", show(n_v' + str(x) + '),'
+        var = str(x)
+        literals += f'var 0..1: v{var}; var 0..1: n_v{var};\n'
+        constraints += f'constraint v{var} + n_v{var} = 1;\n'
+        output += f'"v{var}=" ,show(v{var}), "\\t-v{var}=", show(n_v{var}),'
         if x == n_literals: # Verificar si es la ultima iteracion para delar de poner las comas y poner un ]
-            output += '"v' + str(x) + '=" ,show(v' + str(x) + '), "\\t-v' + str(x) + '=", show(n_v' + str(x) + ')]'
+            output += f'"v{var}=" ,show(v{var}), "\\t-v{var}=", show(n_v{var})]'
 
 
     s.write(literals) # Se escribe en el archivo los literales
--- reducir.sh
+++ reducir.sh
@@ -1,3 +1,4 @@
 apt-get update && apt-get upgrade
 apt-get install python3.6
-python3.6 SAT-IP/Reductor/main.py
+cd SAT-IP/Reductor/
+python3.6 main.py
