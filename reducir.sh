311eac323ee3132720be15eef6567dbbc738cede
Luis Granja <luis.granja@correounivalle.edu.co>
2019-07-24 18:57:08 -0500

    fix: adding -y flag

--- reducir.sh
+++ reducir.sh
@@ -1,4 +1,4 @@
-apt-get install software-properties-common
+apt-get install software-properties-common -y
 add-apt-repository ppa:jonathonf/python-3.6
 apt-get update -y
 apt-get install python3.6 -y
