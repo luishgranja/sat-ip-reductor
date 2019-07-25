c863247a3ec27fbd03c89d4b3f223cb04c882979
Luis Granja <luis.granja@correounivalle.edu.co>
2019-07-25 12:34:34 -0500

    feat: detect if python3.6 is installed

--- reducir.sh
+++ reducir.sh
@@ -1,6 +1,15 @@
-apt-get install software-properties-common -y
-add-apt-repository ppa:jonathonf/python-3.6
-apt-get update -y
-apt-get install python3.6 -y
-cd Reductor/
-python3.6 main.py
+#!/bin/bash
+lines=$(which python3.6|wc -l)
+if [ $lines -eq 1 ];
+then
+    cd Reductor/
+    python3.6 main.py
+else
+    apt-get install software-properties-common -y
+    add-apt-repository ppa:jonathonf/python-3.6
+    apt-get update -y
+    apt-get install python3.6 -y
+    cd Reductor/
+    python3.6 main.py
+fi
+
